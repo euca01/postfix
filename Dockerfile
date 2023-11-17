6f2c2e1ae96faac330c70afef73c2c099f39c3f1
Nicolas BERNA <nicolas_github@berna.fr>
2023-11-17 23:59:18 +0100

    Test

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,11 @@
 FROM alpine:3.18
 
-ENV HOSTNAME=srv01.myhostname.com
-ENV MAILAME=mymailname
-ENV MYDOMAIN=mymailname.com
+RUN apk add --no-cache bash postfix postfix-mysql ca-certificates spamassassin-client
 
+COPY config/ /etc/postfix/
 
-RUN apk add --no-cache bash postfix ca-certificates
-
-# Configurer Postfix
-RUN postconf -e "myhostname = ${HOSTNAME}" && \
-    postconf -e "mail_name = ${MAILAME}" && \
-    postconf -e 'smtpd_banner = $myhostname ESMTP $mail_name' && \
-    postconf -e 'biff = no' && \
-    postconf -e 'append_dot_mydomain = no' && \
-    postconf -e "mydomain = ${MYDOMAIN}"
-
-CMD ["/usr/sbin/postfix", "start-fg"]
+CMD ["/etc/postfix/config/run.sh"] 
+#CMD ["/usr/sbin/postfix", "start-fg"]
 
 EXPOSE 25
 EXPOSE 465
---
+++ config/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+function add_config_value() {
+  local key=${1}
+  local value=${2}
+  # local config_file=${3:-/etc/postfix/main.cf}
+  [ "${key}" == "" ] && echo "ERROR: No key set !!" && exit 1
+  [ "${value}" == "" ] && echo "ERROR: No value set !!" && exit 1
+
+  echo "Setting configuration option ${key} with value: ${value}"
+ postconf -e "${key} = ${value}"
+}
+
+
+
+# Set needed config options
+add_config_value "maillog_file" "/dev/stdout"
+add_config_value "myhostname" ${SERVER_HOSTNAME}
+add_config_value "mydomain" ${DOMAIN}
+add_config_value "mydestination" "${DESTINATION:-localhost}"
+add_config_value "myorigin" '$mydomain'
+
+
+#Start services
+
+# If host mounting /var/spool/postfix, we need to delete old pid file before
+# starting services
+rm -f /var/spool/postfix/pid/master.pid
+
+exec /usr/sbin/postfix -c /etc/postfix start-fg
